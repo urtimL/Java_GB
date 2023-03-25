af90cd2800ca9c5e53394dcffcb473372d445254
timur <t.worck.m@gemail.com>
2023-03-25 22:59:05 +0100

    test

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,3 +1,5 @@
+package lesson1;
+
 import lesson1.Task1;
 import lesson1.Task3;
 
@@ -5,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 //        Task1.Hello();
 //        Task2.hello();
-//        int[] array = new int[] {1,1,0,1,1,1,0,1,1,1,1,0,1};
-//        System.out.println(Task3.getMaxOneCount(array));
+        int[] array = new int[] {1,1,0,1,1,1,0,1,1,1,1,0,1};
+        System.out.println(Task3.getMaxOneCount(array));
     }
 }
